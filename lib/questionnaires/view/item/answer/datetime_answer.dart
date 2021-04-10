e46f3c7276c02a178f715b18f3e6f3cf7229f34a
Tilo <tiloc@users.noreply.github.com>
2021-04-11 01:55:38 +0200

    - Clear button for date fields.

--- lib/questionnaires/view/item/answer/datetime_answer.dart
+++ lib/questionnaires/view/item/answer/datetime_answer.dart
@@ -1,8 +1,8 @@
-import 'package:faiadashu/fhir_types/date_time_picker.dart';
 import 'package:fhir/r4.dart';
 import 'package:fhir/r4/resource_types/clinical/diagnostics/diagnostics.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../fhir_types/date_time_picker.dart';
 import '../../../../fhir_types/fhir_types.dart';
 import '../../../../fhir_types/fhir_types_extensions.dart';
 import '../../../questionnaires.dart';
