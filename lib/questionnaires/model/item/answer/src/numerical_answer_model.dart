0d10cb93d10228f18503611ef433759af0defe35
Tilo <tiloc@users.noreply.github.com>
2021-10-19 22:13:56 +0200

    - Support population of quantity answers from expressions.

--- lib/questionnaires/model/item/answer/src/numerical_answer_model.dart
+++ lib/questionnaires/model/item/answer/src/numerical_answer_model.dart
@@ -332,6 +332,17 @@ class NumericalAnswerModel extends AnswerModel<String, Quantity> {
       unit: unitCoding?.localizedDisplay(locale),
       system: unitCoding?.system,
       code: unitCoding?.code,
+      extension_: (qi.type == QuestionnaireItemType.decimal ||
+              qi.type == QuestionnaireItemType.integer)
+          ? [
+              FhirExtension(
+                url: FhirUri(
+                  'http://hl7.org/fhir/StructureDefinition/questionnaire-unit',
+                ),
+                valueCoding: unitCoding,
+              )
+            ]
+          : null,
     );
 
     responseModel.answers = [filledAnswer];
