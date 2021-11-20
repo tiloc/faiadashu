fb392483b0e5d189a24f73a408219fee2881eee3
Tilo <tiloc@users.noreply.github.com>
2021-11-20 23:41:41 +0100

    - Update script

--- tool/create_diagrams.sh
+++ tool/create_diagrams.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 # Requires dcdg
-# Requires nomnoml-cli
-# Requires 'open' command (tested on macOS Big Sur)
-#mkdir -p build/doc/nomnoml
+# Requires plantuml
+# Requires 'open' command (tested on macOS Big Sur and Monterey)
 mkdir -p build/doc/plantuml
 mkdir -p doc/png
 
-#dart run ../dcdg.dart/bin/dcdg.dart --builder=nomnoml --package=`pwd` --verbose --exclude-private=all --exclude="^fhir" --exclude="^flutter" > doc/nomnoml/classes-all.noml
-#nomnoml -i doc/nomnoml/classes-all.noml -o doc/png/classes-all.png
-#open doc/png/classes-all.png
-
-dart run ../dcdg.dart/bin/dcdg.dart --package="$(dirname "$0")/.." --verbose --exclude-private=all --exclude="^fhir" --exclude="^flutter" > build/doc/plantuml/classes-all.puml
+# This is huge ;-)
+export PLANTUML_LIMIT_SIZE=100000
+dart run ../dcdg.dart/bin/dcdg.dart --package="$(dirname "$0")/.." --exclude-private=all --exclude="^fhir" --exclude="^flutter" > build/doc/plantuml/classes-all.puml
 plantuml -i build/doc/plantuml/classes-all.puml -o ../../../doc/png/classes-all.png
 open doc/png/classes-all.png
