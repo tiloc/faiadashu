07ba47caea61d7feac75191224f2827a60c46853
Tilo <tiloc@users.noreply.github.com>
2021-11-21 00:07:40 +0100

    - class diagram

--- tool/create_diagrams.sh
+++ tool/create_diagrams.sh
@@ -8,5 +8,5 @@ mkdir -p doc/png
 # This is huge ;-)
 export PLANTUML_LIMIT_SIZE=100000
 dart run ../dcdg.dart/bin/dcdg.dart --package="$(dirname "$0")/.." --exclude-private=all --exclude="^fhir" --exclude="^flutter" > build/doc/plantuml/classes-all.puml
-plantuml -i build/doc/plantuml/classes-all.puml -o ../../../doc/png/classes-all.png
+plantuml -i build/doc/plantuml/classes-all.puml -o ../../../doc/png
 open doc/png/classes-all.png
